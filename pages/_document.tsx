5b65f1516484231f8e0e39ca57874397860d2597
AbbasJafari-vu <abbas.jafari.4001@gmail.com>
2024-01-31 11:47:28 +0330

    edit fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
     return (
         <Html dir="rtl">
-            <Head />
+            <Head>
+                <link rel="stylesheet" href="/fonts/dana/dana.css" />
+                <link rel="stylesheet" href="/fonts/peyda/peyda.css" />
+            </Head>
             <body>
                 <Main />
                 <NextScript />
